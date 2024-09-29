ec02306ce13352105fe168f0fa79e74e7a1ebfee
Surojit Paul <surojit99923@gmail.com>
2024-09-29 20:29:15 +0530

    j

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 mod easy;
 mod easy_b;
 
-
 fn main() {
+    hq9_test();
+}
+
+// a133
+fn hq9_test() {
+    let mut inp = String::with_capacity(10);
+    std::io::stdin().read_line(&mut inp).unwrap();
+
+    let mut dx = false;
+    for c in inp.trim().chars() {
+        if c == 'H' || c == 'Q' || c == '9' {
+            dx = true
+        }
+    }
+    println!("{}", if dx { "YES" } else { "NO" });
 }
